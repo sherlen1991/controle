fe17033ef7bfe731d249ca27b1eaaa2cdea1cfc5
sherlen1991 <sherlenalves33@gmail.com>
2026-05-15 06:39:52 -0300

    a

--- android-app/app/src/main/java/com/controleapp/MainActivity.java
+++ android-app/app/src/main/java/com/controleapp/MainActivity.java
@@ -1,27 +1,44 @@
 package com.controleapp;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    private SwipeRefreshLayout swipeRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        swipeRefresh = findViewById(R.id.swipeRefresh);
         webView = findViewById(R.id.webView);
+        
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
 
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeRefresh.setRefreshing(false);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient());
+
+        swipeRefresh.setOnRefreshListener(() -> {
+            webView.reload();
+        });
+
         webView.loadUrl("file:///android_asset/index.html");
     }
 
